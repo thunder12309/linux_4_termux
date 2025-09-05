b967d564045ff4206ff8ab2b5de140a013bf30a9
thunder12309 <uscience646@gmail.com>
2025-09-05 12:13:08 +0530

    script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+./script.sh
+echo "your installation process in being started"
+pkg update &
+pkg install proot-distro &
+pd list
+echo "choose os and type 'pd install {name}' eg:- pd install ubuntu after installation login into os"
+
